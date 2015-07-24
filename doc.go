76763d621da79660c1d72e3bb8d8196522f5171c
Arthur White <arthur@whitedevops.com>
2015-07-24 15:43:38 +0200

    Set GoDoc

--- doc.go
+++ doc.go
@@ -1,74 +1,26 @@
 /*
-Package assets is a helper for the Core.
-It provides syntactic sugar that let's you easily write responses on *core.Context.
+Package static is a handler for the Core.
+It serves files from the "static" directory on the "/static" URL path.
 
 Usage
 
-Full example with status, string, bytes, JSON and view responses:
+Example:
 
 	package main
 
 	import (
-		"net/http"
-		"strings"
-
 		"github.com/volatile/core"
-		"github.com/volatile/response"
-		"github.com/volatile/route"
+		"github.com/volatile/static"
 	)
 
 	func main() {
-		// We set functions for views templates.
-		response.ViewsFuncs(response.FuncMap{
-			"toUpper": strings.ToUpper,
-			"toLower": strings.ToLower,
-		})
-
-		// Status response
-		route.Get("^/status$", func(c *core.Context) {
-			response.Status(c, http.StatusForbidden)
-		})
-
-		// String response
-		route.Get("^/string$", func(c *core.Context) {
-			response.String(c, "Hello, World!")
-		})
-
-		// Bytes response
-		route.Get("^/bytes$", func(c *core.Context) {
-			response.Bytes(c, []byte("Hello, World!"))
-		})
-
-		// JSON response
-		route.Get("^/json$", func(c *core.Context) {
-			response.JSON(c, &Car{
-				ID:    1,
-				Brand: "Bentley",
-				Model: "Continental GT",
-			})
-		})
-
-		// View response
-		route.Get("^/(?P<name>[A-Za-z]+)$", func(c *core.Context, params map[string]string) {
-			response.View(c, "hello", params)
-		})
+		static.Use(static.DefaultMaxAge)
 
 		core.Run()
 	}
 
-	type Car struct {
-		ID    int    `json:"id"`
-		Brand string `json:"brand"`
-		Model string `json:"model"`
-	}
+Cache "max-age"
 
-Views
-
-The views templates are recursively parsed from the "views" directory, just before running the server.
-The file names and extensions and the directory organization doesn't matter. All the files are parsed.
-
-Template functions
-
-To use functions in your views, use response.ViewsFuncs(response.FuncMap{}), like with the `html/template` standard package.
+The "static.Use" method receives a single parameter: the "max-age" (in seconds) for all the static files.
 */
 package static
